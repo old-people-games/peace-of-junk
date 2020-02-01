ba8f4012d4642d926ce06a25b5f959517ce64957
bananani <haraldurinn@gmail.com>
2020-02-01 16:28:32 +0200

    changed the character controller and camera controller and tuned character movement to be better

--- peace-of-junk/Assets/Scripts/CharacterController.cs
+++ peace-of-junk/Assets/Scripts/CharacterController.cs
@@ -5,46 +5,50 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    enum stateIdentifier
-    {
-        idle = 0,
-        Walking = 1,
-        Jumping = 2,
-        falling = 3,
-    }
-
-    public float speed;
-    private stateIdentifier state = stateIdentifier.idle;
     private Rigidbody2D rb2d;
 
+    public float MaxSpeed;
     public float jumpAddedByHolding;
     public float maxJumpAmount;
     private float _jumpForce;
+    private BoxCollider2D collider;
+    private Collision2D _platformCollision;
 
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        _platformCollision = other;
+    }
+
+    Vector3 velocity;
+    float accelerationScalar;
+    float maxSpeed;
+    float friction;
 
 
     void FixedUpdate()
     {
-        if (Input.GetAxis("Horizontal") > 0.001f)
+        if (_platformCollision == null)
         {
-            state = stateIdentifier.Walking;
-        }
-        else
-        {
-            state = stateIdentifier.idle;
+            return;
         }
 
 
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        if (moveHorizontal == 0f)
+        transform.localScale = moveHorizontal < 0 ? new Vector3(-1, 1, 1) : new Vector3(1, 1, 1);
+
+
+        if (collider.IsTouching(_platformCollision.collider) && Input.GetButtonUp("Jump") && _jumpForce > 0)
         {
-            rb2d.inertia = 0;
+            rb2d.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            _jumpForce = 0;
         }
 
         if (Input.GetButton("Jump") && _jumpForce <= maxJumpAmount)
@@ -52,13 +56,11 @@ public class CharacterController : MonoBehaviour
             _jumpForce += jumpAddedByHolding;
         }
 
-        if (Input.GetButtonUp("Jump") && _jumpForce > 0)
+        if (Input.GetButton("Jump"))
         {
-            rb2d.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
-            _jumpForce = 0;
+            return;
         }
 
-        Vector2 movement = new Vector2(moveHorizontal, 0);
-         rb2d.AddRelativeForce(movement * speed);
-         }
+        rb2d.velocity = new Vector2(moveHorizontal * MaxSpeed, rb2d.velocity.y);
+    }
 }
