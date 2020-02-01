8833009db082f0e63eb8ceb3eca179a2c97b0808
lauripalonen <palonen.lauri@gmail.com>
2020-02-01 18:01:04 +0200

    make player to die from kkilling blogs

---
+++ peace-of-junk/Assets/Scrip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrip : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")){
+            var cController = other.GetComponent<CharacterController>();
+
+            if(cController == null){
+                return;
+            }
+
+            cController.Die();
+        }
+    }
+}
