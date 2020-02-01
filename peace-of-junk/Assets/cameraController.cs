680fb50108171fb6ab35f66da4b40b1c40a3b383
lauripalonen <palonen.lauri@gmail.com>
2020-02-01 14:23:27 +0200

    add camera controller

---
+++ peace-of-junk/Assets/cameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    public Transform cameraTransform;
+    public Vector3 cameraOffset;
+    public bool focusOnPlayer;
+
+    void Start()
+    {
+        if(cameraTransform == null){
+            cameraTransform = gameObject.transform;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(!focusOnPlayer){
+            return;
+        }
+        cameraTransform.position = playerTransform.position + cameraOffset;
+    }
+}
