dce05a6221d407f7ed9b3d605e51842d9f73258f
bananani <haraldurinn@gmail.com>
2020-02-01 14:03:20 +0200

    added stupid jump

--- peace-of-junk/Assets/Scripts/CharacterController.cs
+++ peace-of-junk/Assets/Scripts/CharacterController.cs
@@ -4,29 +4,56 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float speed;
+    enum stateIdentifier
+    {
+        idle = 0,
+        Walking = 1,
+        Jumping = 2,
+        falling = 3,
+    }
 
+    public float speed;
+    private stateIdentifier state = stateIdentifier.idle;
     private Rigidbody2D rb2d;
 
-   
+    public float jumpAddedByHolding;
+    public float maxJumpAmount;
+    private float _jumpForce;
+
+
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
 
-   
+
     void FixedUpdate()
     {
-       
+        if (Input.GetAxis("Horizontal") > 0.001f)
+        {
+            state = stateIdentifier.Walking;
+        }
+        else
+        {
+            state = stateIdentifier.idle;
+        }
+
+
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-       
-        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetButton("Jump") && _jumpForce <= maxJumpAmount)
+        {
+            _jumpForce += jumpAddedByHolding;
+        }
 
-       
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        if (Input.GetButtonUp("Jump") && _jumpForce > 0)
+        {
+            rb2d.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            _jumpForce = 0;
+        }
 
-        
-        rb2d.AddForce(movement * speed, ForceMode2D.Impulse);
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddRelativeForce(movement * speed, ForceMode2D.Force);
     }
 }
