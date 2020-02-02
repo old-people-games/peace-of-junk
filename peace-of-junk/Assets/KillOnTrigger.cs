1d2b4ae1bdcd77c9d7b52850e9c241161b11ace7
bananani <haraldurinn@gmail.com>
2020-02-02 13:14:12 +0200

    added propeller to prefab folder with animations

---
+++ peace-of-junk/Assets/KillOnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var player = other.GetComponent<CharacterController>();
+            player.Die();
+        }
+    }
+}
