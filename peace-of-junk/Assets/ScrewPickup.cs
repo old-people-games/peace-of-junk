1576d0df38b31414ddef9db2a9f54aad94f0da70
bananani <haraldurinn@gmail.com>
2020-02-02 12:16:30 +0200

    added collaectable

---
+++ peace-of-junk/Assets/ScrewPickup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrewPickup : MonoBehaviour
+{
+    public GameObject ParticleEffect;
+    public SpawnPoint SpawnPoint;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+
+        Instantiate(ParticleEffect, transform.position, transform.rotation);
+        SpawnPoint.SetSpawn(transform);
+        Destroy(gameObject);
+    }
+}
