4a5c4794753bd6543d1cc8a1c3c4c2bfcd261973
bananani <haraldurinn@gmail.com>
2020-02-02 13:01:03 +0200

    updated bird

---
+++ peace-of-junk/Assets/BirdScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+   public Animator animator;
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if(!other.CompareTag("Player"))
+      {
+         return;
+      }
+      animator.SetTrigger("Win");
+   }
+}
